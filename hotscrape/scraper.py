bff2c9a620a9ecc216edb2862b39010db50d699b
Jakob Calvén <jakob.calven@gmail.com>
2020-05-28 22:59:38 -0700

    Add columns to attributes_dict

--- hotscrape/scraper.py
+++ hotscrape/scraper.py
@@ -33,7 +33,7 @@ class Scraper(object):
     #def 
 
 
-def get_hotels_page(url):    
+def get_hotels_page(url, max_scroll=1):    
     
     '''
     Takes an url from Hotels.com and 
@@ -70,13 +70,12 @@ def get_hotels_page(url):
         if any([cur_elem.is_displayed() for cur_elem in driver.find_elements_by_class_name("info")]):
             break
 
-        if scroll_count > 20:
+        if scroll_count > max_scroll:
             break
             
     # Grabs the html of the fully scrolled-down page and parse it with BeautifulSoup  
     # innerHTML = driver.execute_script('return document.body.innerHTML')
-    innerHTML = driver.page_source
-    parsed_html = BeautifulSoup(innerHTML, 'lxml')
+    parsed_html = BeautifulSoup(driver.page_source, 'lxml')
     driver.close()
     return parsed_html
 
@@ -84,7 +83,7 @@ def generate_url(destination, checkin_date, checkout_date=None, price_min=0, pri
                  star_rating_min=1, star_rating_max=5, guest_rating_min=1, guest_rating_max=9, distance_centre=None, 
                  rooms=1, adults=2, children=0, currency="USD"):
 
-#https://www.hotels.com/search.do?resolved-location=CITY%3A1504033%3AUNKNOWN%3AUNKNOWN&f-price-currency-code=USD&f-price-multiplier=1&f-price-min=30&f-price-max=395&f-star-rating=5,4,3,2,1&f-guest-rating-min=2&f-guest-rating-max=9&f-distance=2.0&f-lid=1504033&destination-id=1504033&q-destination=Las%20Vegas,%20Nevada,%20United%20States%20of%20America&q-check-in=2020-05-13&q-check-out=2020-05-14&q-rooms=1&q-room-0-adults=2&q-room-0-children=0&sort-order=DISTANCE_FROM_LANDMARK
+    #https://www.hotels.com/search.do?resolved-location=CITY%3A1504033%3AUNKNOWN%3AUNKNOWN&f-price-currency-code=USD&f-price-multiplier=1&f-price-min=30&f-price-max=395&f-star-rating=5,4,3,2,1&f-guest-rating-min=2&f-guest-rating-max=9&f-distance=2.0&f-lid=1504033&destination-id=1504033&q-destination=Las%20Vegas,%20Nevada,%20United%20States%20of%20America&q-check-in=2020-05-13&q-check-out=2020-05-14&q-rooms=1&q-room-0-adults=2&q-room-0-children=0&sort-order=DISTANCE_FROM_LANDMARK
 
     # Concatenate star rating list into string of correct format
     star_rating = "".join(map(lambda x: str(x)+",", range(star_rating_max,star_rating_min-1,-1))).rstrip(",")
@@ -120,7 +119,7 @@ def generate_url(destination, checkin_date, checkout_date=None, price_min=0, pri
 
 feature_html_details = {'name': ('h3', 'p-name'),
                         'address': ('span', 'address'),
-                        'maplink': ('a', 'map-link xs-welcome-rewards'),
+                        # 'maplink': ('a', 'map-link xs-welcome-rewards'),
                         'landmarks': ('ul', 'property-landmarks'),
                         'amenities': ('ul', 'hmvt8258-amenities'),
                         'details': ('div', 'additional-details resp-module'),
@@ -137,14 +136,20 @@ def get_content_list(soup, tag, class_):
     return raw_list
 
 
-def get_attributes(soup):
+def get_attributes(soup, **search_dict):
 
     attributes_dict = {key: get_content_list(soup, feature_html_details[key][0], feature_html_details[key][1]) for key in feature_html_details}
     n_hotels = len(attributes_dict["name"])
-    # attributes_dict["checkin"] = [checkin] * n_hotels
-    # attributes_dict["checkout"] = [checkout] * n_hotels
-    # attributes_dict["adults"] = [adults] * n_hotels
-    # attributes_dict["children"] = [children] * n_hotels
+
+    checkin_date = search_dict.get("checkin_date")
+    checkout_date = search_dict.get("checkout_date")
+    if checkout_date is None:
+        if checkin_date is not None:
+            checkout_date = (pd.to_datetime(checkin_date) + pd.DateOffset(1)).strftime("%Y-%m-%d")
+    attributes_dict["checkin_date"] = [checkin_date] * n_hotels
+    attributes_dict["checkout_date"] = [checkout_date] * n_hotels
+    attributes_dict["adults"] = [search_dict.get("adults")] * n_hotels
+    attributes_dict["children"] = [search_dict.get("children")] * n_hotels
 
     #print(f'{checkin} and {checkout} is successful!', hotel_df.shape)
     return attributes_dict
@@ -215,5 +220,5 @@ if __name__ == '__main__':
 
     url = generate_url(**search_dict)
     soup = get_hotels_page(url)
-    res = get_attributes(soup)
+    res = get_attributes(soup, **search_dict)
     #raw_hotel_2_0, souppp = combine_df(search_dict)
