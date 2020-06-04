b151471daa09f00d33c5968395416e5a53726862
Jakob Calvén <jakob.calven@gmail.com>
2020-06-03 21:41:39 -0700

    Added sql test module

---
+++ tests/test_sql.py
@@ -0,0 +1,21 @@
+from hotscrape import sql
+from tests.test_base import *
+
+class TestSQL(TestBase):
+
+    def test_db_upsert(self):
+
+        search_dict = self.search_dict
+        schema = self.schema
+
+        search_dict_ = hs.ensure_search_format(search_dict)
+        url = hs.generate_url(**search_dict_)
+        soup = hs.get_hotels_page(url)
+        res = hs.get_attributes(soup, **search_dict_)
+
+        df_search, df_attributes = hs.get_dfs(search_dict_, res)
+        conn = sql.create_database("./test_sql", schema)
+
+        sql.to_sql(df_search, "search", conn)
+        sql.to_sql(df_attributes, "hotels", conn)
+
