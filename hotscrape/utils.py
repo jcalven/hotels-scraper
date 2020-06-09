bc142945661bd4a0b5534dcbec270d4839b9cb9c
Jakob Calvén <jakob.calven@gmail.com>
2020-06-09 10:11:09 -0700

    Added function read search config file

--- hotscrape/utils.py
+++ hotscrape/utils.py
@@ -1,4 +1,5 @@
 import yaml
+from configparser import ConfigParser
 
 def load_schema(filename="db_schema.yml", path="."):
     with open(f"{path}/{filename}") as file:
@@ -6,3 +7,9 @@ def load_schema(filename="db_schema.yml", path="."):
         # scalar values to Python the dictionary format
         schema = yaml.load(file, Loader=yaml.FullLoader)
     return schema
+
+def read_search_config(search_file):
+        config = ConfigParser()
+        config.read(search_file)
+        config.sections()
+        return [dict(config.items(s)) for s in config.sections()]
